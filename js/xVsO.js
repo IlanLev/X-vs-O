67284fa5bb3a116657d61337a47a3325889f815f
IlanLev <36135147+IlanLev@users.noreply.github.com>
2018-08-05 15:43:40 +0200

    x

---
+++ js/xVsO.js
@@ -0,0 +1,12 @@
+
+var c1 =document.getElementById("c1");
+
+c1.onclick = function(){c1.innerHTML = "X"};
+document.getElementById("c2").onclick = function(){alert("c2")};
+document.getElementById("c3").onclick = function(){alert("c3")};
+document.getElementById("c4").onclick = function(){alert("c4")};
+document.getElementById("c5").onclick = function(){alert("c5")};
+document.getElementById("c6").onclick = function(){alert("c6")};
+document.getElementById("c7").onclick = function(){alert("c7")};
+document.getElementById("c8").onclick = function(){alert("c8")};
+document.getElementById("c9").onclick = function(){alert("c9")};
